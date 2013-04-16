b495dfd3c4c07294d72aadbb3c80c5a0090d2cdd
Dongxu Huang <huang@wandoujia.com>
2013-04-16 16:28:17 +0800

    fix bug

--- template/dropWindow/js/manage.js
+++ template/dropWindow/js/manage.js
@@ -21,33 +21,6 @@ $(function() {
     var spinner = new Spinner(opts).spin(target);
 
     var pn = null;
-    // 全部应用的翻页
-    $('.allapps-content .pagination li').live('click',
-    function() {
-        var page = parseInt($(this).find('a').text());
-
-        $('.allapps-content .pagination li').removeClass('current');
-        $(this).addClass('current');
-
-        $('.allapps-content .carousel').fadeOut(100);
-        $('.allapps-content .carousel').eq(page - 1).fadeIn(100);
-
-         var cur_page = parseInt($('.allapps-content .pagination .current').find('a').text()) || 1,
-        total_page = parseInt($('.allapps-content .pagination li').last().find('a').text()) || 1;
-
-        if (cur_page == 1) {
-            $('.prev').hide();
-            $('.next').show();
-        }
-        else if (cur_page == total_page) {
-            $('.next').hide();
-            $('.prev').show();
-        } else {
-            $('.prev').show();
-            $('.next').show();
-        }
-
-    });
 
     // 全部应用的删除
     $('.allapps-content .app .unstall').live('click',
@@ -135,15 +108,16 @@ function HideSpin(){
 
 function RemoveAppByPn(pn) {
     appRemove($('#app_' + hex_md5(pn) + ' .unstall'), 20);
-    appRemove($('#update_app_' + hex_md5(pn) + ' .unstall'),12);
+    appRemove($('#update_app_' + hex_md5(pn) + ' .update'),12);
 }
 
 function RemoveUpdateAppByPn(pn) {
-    appRemove($('#update_app_' + hex_md5(pn) + ' .unstall'), 12);
+    appRemove($('#update_app_' + hex_md5(pn) + ' .update'), 12);
 }
 
 function JumpToFirstPage() {
-    $('.allapps-content .pagination li').first().click();
+    // TODO
+    //$('.allapps-content .pagination li').first().click();
 }
 // 添加app
 function AddApp(json) {
@@ -162,9 +136,7 @@ function AddApp(json) {
                         </li>';
 
     $('.downloaded .original').append(insert_app);
-    $('.slides.downloaded').miniSlides({
-        count : 20
-    });
+    
 }
 
 // 删除app
@@ -220,31 +192,7 @@ function RemoveApp(obj) {
 }
 
 function RemoveAllApps(){
-
-    $('.allapps-content .pagination').html('');
-    $('.update-content .pagination').html('');
-
-    $('.allapps-content .carousel').each(function(i){
-
-        if ( i == 0 ){
-            $(this).find('ul').html('');
-        }else{
-            $(this).remove();
-        }
-
-    });
-
-    $('.update-content .carousel').each(function(i){
-        if ( i == 0 ){
-            $(this).find('ul').html('');
-        }else{
-            $(this).remove();
-        }
-    });
-
-    $('.update-content .update-all').remove();
-    $('.next').hide();
-    $('.prev').hide();
+    // TODO
 }
 
 
@@ -261,174 +209,13 @@ function AddUpdateApp(json) {
                         <a class="show-unstall" href="#" hideFocus="true">\
                         <img src="' + json.icon + '" class="icon" width="68" data-pn="' + json.pn + '" />\
                         <span class="name" title="' + json.name + '">' + decodeURIComponent(json.name) + '</span>   \
-                        <a href="app:update:'+ json.url +'#pn='+ json.pn +'&name='+decodeURIComponent(json.name)+'&icon='+json.icon+'" class="update" onClick="$(\'.down-tab\').click();" hideFocus="true" data-url="' + json.url +'">升级</a> \
+                        <a href="app:update:'+ json.url +'#pn='+ json.pn +'&name='+decodeURIComponent(json.name)+'&icon='+json.icon+'" class="update" onClick="$(\'.down-nav\').click();" hideFocus="true" data-url="' + json.url +'">升级</a> \
                         </a>\
                         </li>';
 
     $('.need-update .original').append(insert_app);
-    $('.slides.need-update').miniSlides({
-         count : 12
-    });
 }
-
-// 删除 update app
-function RemoveUpdateApp(obj) {
-
-    var cur_page = parseInt($('.update-content .pagination .current').find('a').text()) || 1,
-    total_page = parseInt($('.update-content .pagination li').last().find('a').text()) || 1,
-    next_page,
-    cur_container = $(obj).closest('.carousel');
-
-    $(obj).closest('.app').remove();
-    // 如果删的不是最后一页
-    if (cur_page != total_page) {
-        if (cur_container.find('.app').length == 0) {
-            cur_container.remove();
-            $('.update-content .pagination .current').remove();
-
-            next_page = cur_page + 1;
-
-            var page_li = $('.update-content .pagination li');
-
-            page_li.eq(next_page - 1).click();
-
-            for (var i = 0; i < page_li.length; i++) {
-                page_li.eq(i).find('a').text(i + 1);
-            }
-
-        }
-    }
-    // 如果删的是最后一页
-    else {
-        if (cur_container.find('.app').length == 0) {
-
-            // 还剩最后一页
-            if (total_page == 1) {
-                cur_container.find('ul').html('');
-            } else {
-                cur_container.remove();
-            }
-
-            // 总共还剩2页
-            if (total_page == 2) {
-                $('.update-content .pagination').html('');
-                $('.update-content .carousel').show();
-            }
-
-            $('.update-content .pagination .current').remove();
-            next_page = cur_page - 1;
-            var page_li = $('.update-content .pagination li');
-            page_li.eq(next_page - 1).click();
-
-        }
-    }
-
-}
-function jumpToNextPage(){
-
-    var allapps = $('.allapps-content'),
-    updateapps = $('.update-content'),
-    pagination ;
-
-    if ( allapps.css('display') == 'block' ){
-        pagination = allapps.find('.pagination');
-    }else{
-        pagination = updateapps.find('.pagination');
-    }
-
-    var cur_page = parseInt(pagination.find('.current').find('a').html()),
-    total_page = parseInt(pagination.find('li').last().find('a').html()),
-    next_page ;
-
-    if ( cur_page == total_page ){
-        next_page = cur_page;
-    }else if( cur_page == 1 && total_page > 1){
-        next_page = 2;
-    }else{
-        next_page = cur_page + 1;
-    }
-
-    pagination.find('li').eq(next_page - 1).click();
-
-}
-
-function jumpToPrevPage(){
-
-    var allapps = $('.allapps-content'),
-    updateapps = $('.update-content'),
-    pagination ;
-
-    if ( allapps.css('display') == 'block' ){
-        pagination = allapps.find('.pagination');
-    }else{
-        pagination = updateapps.find('.pagination');
-    }
-
-    var cur_page = parseInt(pagination.find('.current').find('a').html()),
-    prev_page ;
-
-    if ( cur_page == 1 ){
-        prev_page = cur_page;
-    }else{
-        prev_page = cur_page - 1;
-    }
-
-    pagination.find('li').eq(prev_page - 1).click();
-}
-
 
 $(function() {
-    $('.down-tab').on('click',
-    function() {
-        $('.title').html("下载管理");
-        $(this).addClass('down-tab-current');
-        $('.apps-tab').removeClass('apps-tab-current');
-        $('.up-tab').removeClass('up-tab-current');
-        $('.task-content').show();
-        $('.update-content').hide();
-        $('.allapps-content').hide();
-    });
-    $('.apps-tab').on('click',
-    function() {
-        $('.title').html("我的应用");
-        $(this).addClass('apps-tab-current');
-        $('.down-tab').removeClass('down-tab-current');
-        $('.up-tab').removeClass('up-tab-current');
-        $('.task-content').hide();
-        $('.update-content').hide();
-        $('.allapps-content').show();
-    });
-    $('.up-tab').on('click',
-    function() {
-        $('.title').html("应用升级")
-        $(this).addClass('up-tab-current');
-        $('.down-tab').removeClass('down-tab-current');
-        $('.apps-tab').removeClass('apps-tab-current');
-        $('.task-content').hide();
-        $('.update-content').show();
-        $('.allapps-content').hide();
-    });
-
-    $('.next').live('click', function(){
-        var cur_page = parseInt($('.allapps-content .pagination .current').find('a').text()) || 1,
-        total_page = parseInt($('.allapps-content .pagination li').last().find('a').text()) || 1;
-
-        jumpToNextPage();
-        if (cur_page == total_page - 1) {
-            $(this).hide();
-        }
-        $('.prev').show();
-
-    });
-    $('.prev').live('click', function(){
-        jumpToPrevPage();
-
-        var cur_page = parseInt($('.allapps-content .pagination .current').find('a').text()) || 1,
-        total_page = parseInt($('.allapps-content .pagination li').last().find('a').text()) || 1;
-
-        if (cur_page == 1) {
-            $(this).hide();
-        }
-        $('.next').show();
-    })
+    
 });
